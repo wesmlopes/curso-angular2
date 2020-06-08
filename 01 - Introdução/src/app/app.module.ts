8a0753f0cd126be9a877a01ae593c25cd26620a9
Wesley de Moraes Lopes <wesmlopes@icloud.com>
2020-06-08 03:16:02 -0300

    primeiro projeto - componentes / módulos / templates e serviços

---
+++ 01 - Introdução/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+// import { FormsModule } from '@angular/forms'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component'
+import { CursosModule } from './cursos/cursos.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiro2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CursosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
